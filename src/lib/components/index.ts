eb625a645cd0eab4f258bfa14e36b482604ef1bb
nverma61 <namanverma2024@gmail.com>
2025-02-08 13:19:00 -0500

    pop-ups

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -4,3 +4,5 @@ export { default as Breadcrumbs } from './ui/Breadcrumbs.svelte';
 export {default as FeatureCard } from './FeatureCard.svelte';
 export { default as Navbar } from './NavBar.svelte';
 export { default as SideBar } from './SideBar.svelte';
+export { default as BadgeAchievementPopup } from './ui/BadgeAchievementPopup.svelte';
+export { default as FeedbackPopup } from './ui/FeedbackPopup.svelte';
