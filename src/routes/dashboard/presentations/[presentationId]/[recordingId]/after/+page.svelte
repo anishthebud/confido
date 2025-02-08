cae084f0338522aa57ca32e5b9dfa332f0ba7787
anishthebud <97369302+anishthebud@users.noreply.github.com>
2025-02-08 16:03:02 -0500

    worked on the backend for the badges

--- src/routes/dashboard/presentations/[presentationId]/[recordingId]/after/+page.svelte
+++ src/routes/dashboard/presentations/[presentationId]/[recordingId]/after/+page.svelte
@@ -1,5 +1,30 @@
-<script lang="ts">
-	import type { PageServerData } from './$types';
+<script>
+    import { BadgeAchievementPopup } from "$lib/components";
+    import { FeedbackPopup} from "$lib/components";
+  
+    let showingBadges = true;
+    let showingFeedback = false;
 
-	let { data }: { data: PageServerData } = $props();
-</script>
+    let { data } = $props();
+    
+    function handleShowFeedback() {
+      showingBadges = false;
+      showingFeedback = true;
+    }
+  </script>
+  
+  {#if showingBadges}
+    <BadgeAchievementPopup 
+      score={data.scoreInt}
+      presentationCount={3}
+      onShowFeedback={handleShowFeedback}
+      earnedBadges={data.earnedBadges}
+    />
+  {/if}
+  
+  {#if showingFeedback}
+    <FeedbackPopup 
+      score={data.scoreInt}
+      feedback={data.comments}
+    />
+  {/if}
