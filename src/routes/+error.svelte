76382e9f14e9e00a26f187e2b58681af7fbe1dc9
Rohan Godha <git@rohangodha.com>
2025-02-09 01:58:57 -0500

    404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<main class="flex flex-col min-h-screen bg-gradient-to-b from-gray-400 to-white">
+	<div class="flex flex-col flex-grow justify-center items-center w-full">
+		<div class="flex flex-col gap-y-2 items-center">
+			<h3 class="">{page.error?.message}</h3>
+			<h4 class="">We're working on fixing this</h4>
+		</div>
+	</div>
+</main>
