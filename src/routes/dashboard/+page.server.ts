dd53a587e3ca65561f8e47ba5807cf4c1addddcd
idke64 <deliciousbrain42@gmail.com>
2025-02-09 03:08:36 -0500

    fix signin redirects

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -4,5 +4,7 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ locals: { user } }) => {
 	if (!user) {
 		redirect(303, '/auth/login');
+	} else {
+		redirect(303, '/dashboard/profile');
 	}
 };
